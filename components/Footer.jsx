60bce50ee91359f772d246b264953a591bb6873f
tunny17 <tvnji17@gmail.com>
2023-05-21 13:50:53 +0100

    - Changed Footer message - Adjusted product-detail-container margin

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 JSM Headphones All rights reserverd</p>
+      <p>2023 Adeyemi Adetunji All rights reserved</p>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
